4da69f930a6deca21f98c93b0496fe11be4aa8da
Vargen18 <widens@student.chalmers.se>
2018-12-06 17:03:19 +0100

    Moved relevant methods from CarController to CarModel

--- src/Model/CarModel.java
+++ src/Model/CarModel.java
@@ -1,6 +1,71 @@
 package Model;
 
+import Observer.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CarModel {
 
+    private final static ArrayList<Cars> cars = new ArrayList<>();
+    List<Observer> observers = new ArrayList<Observer>();
+
+    public void gas(int amount) {
+        double gas = ((double) amount) / 100;
+        for (Cars car : cars) {
+            car.gas(gas);
+        }
+
+    }
+    public void brake(int amount) {
+        double brake = ((double) amount) / 100;
+        for (Cars car : cars
+        ) {
+            car.brake(brake);
+        }
+    }
+    public void startEngine(){
+        for (Cars car : cars){
+            car.startEngine();
+        }
+    }
+    public void stopEngine(){
+        for (Cars car : cars){
+            car.stopEngine();
+        }
+    }
+    public void tiltDown(){
+        for (Cars car : cars){
+            if (car instanceof Scania){
+                ((Scania) car).tiltDown();
+            }
+        }
+    }
+    public void tiltUp(){
+        for (Cars car : cars){
+            if (car instanceof Scania){
+                ((Scania) car).tiltDown();
+            }
+        }
+    }
+    public void setTurboOn(){
+        for (Cars car : cars){
+            if (car instanceof Saab95){
+                ((Saab95) car).setTurboOn();
+            }
+        }
+    }
+    public void setTurboOff(){
+        for (Cars car : cars){
+            if (car instanceof Saab95){
+                ((Saab95) car).setTurboOff();
+            }
+        }
+    }
+
+    public static ArrayList<Cars> getCars() {
+        return cars;
+    }
 
 }
