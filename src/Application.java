7d4d01335f171e26344d3eaefe6523325559c47c
Vargen18 <widens@student.chalmers.se>
2018-12-06 17:05:09 +0100

    Moved relevant methods from CarController to Application.

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,51 @@
+import Controller.CarController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Application {
+
+
+    // The delay (ms) corresponds to 20 updates a sec (hz)
+    private final int delay = 50;
+    // The timer is started with an listener (see below) that executes the statements
+    // each step between delays.
+    private Timer timer = new Timer(delay, new TimerListener());
+
+
+    public static void main(String[] args) {
+        // Instance of this class
+        CarController cc = new CarController();
+
+
+        cc.cars.add(new Volvo240());
+        cc.cars.add(new Saab95());
+        cc.cars.add(new Scania());
+
+        // Start a new view and send a reference of self
+        cc.frame = new CarView("CarSim 1.0", cc);
+
+        // Start the timer
+        cc.timer.start();
+    }
+
+    /* Each step the TimerListener moves all the cars in the list and tells the
+     * view to update its images. Change this method to your needs.
+     * */
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int count = 0;
+            for (Cars car : cars) {
+                car.move();
+                int x = (int) Math.round(car.getPosX());
+                int y = (int) Math.round(car.getPosY());
+                frame.drawPanel.moveit(x, y, count);
+                // repaint() calls the paintComponent method of the panel
+                frame.drawPanel.repaint();
+                count++;
+            }
+        }
+    }
+
 }
